dee44dbfc7f2a67576710c14681bb6702dbde96e
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-04-04 00:09:40 +0600

    added login and registration form

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-50 column-offset-25">
+                <h1>Sign In</h1>
+                <form action="">
+                    <label for="">Email</label>
+                    <input type="email" name="email">
+                    <label for="">Password</label>
+                    <input type="password" name="password">
+                    <button type="submit" class="button-primary">Login</button>
+                    <span>Not Registered Yet!</span> | 
+                    <a href="register.php"><b>Sign Up Here</b></a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
